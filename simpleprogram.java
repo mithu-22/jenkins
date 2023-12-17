642c7e22d794794dd983ea5ebf11e702352dce46
mithra <mithra@hamiltonianusa.com>
2023-12-17 16:57:26 +0530

    build trigger

--- simpleprogram.java
+++ simpleprogram.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+        //Build trigger
         // Creates a reader instance which takes
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
